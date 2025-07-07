3e745f37fc26b43b9ca8d36c5db212fba46c2cf0
AlunoCE <klaiveraluno@gmail.com>
2025-07-07 16:33:14 -0300

    Delete Java/Trabalho-2/src/Sistema de Controle do Cinema directory

--- Java/Trabalho-2/src/Sistema de Controle do Cinema/Cinema/Cinema.java
+++
@@ -1,8 +0,0 @@
-public interface Cinema {
-
-    String getNome();
-    String getEndereco();
-    public void adicionarSala(Sala sala);
-    public void removerSala(Sala sala);
-    public void programarSessao(Filme filme, Sala sala, Sessao sessao);
-}
